289fac9b58ed98e69d9dc38328d9ae5f3975d10f
Naradhipa Bhary <mydhipa@gmail.com>
2022-06-12 00:40:08 +0700

    Handle no data

--- get.py
+++ get.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
 from pydantic import BaseModel
 from starlette.responses import JSONResponse
 
@@ -15,6 +15,8 @@ class UpdateDTO(BaseModel):
 @app.get("/read/{npm}", response_model=UpdateDTO)
 async def say_hello(npm: str):
     data = get_data(npm)
+    if data is None:
+        raise HTTPException(status_code=404, detail="No data found with specified NPM")
     return {
         "NPM": data.npm,
         "name": data.name
