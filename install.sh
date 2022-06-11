a32a4ed7e4994b3074284ac53f0a3277ae75ed16
Naradhipa Bhary <mydhipa@gmail.com>
2022-06-11 23:52:21 +0700

    Update install script

--- install.sh
+++ install.sh
@@ -18,15 +18,15 @@ pip3 install -r requirements.txt
 
 echo "Installing Services"
 
-sed -e "s:<pwd>:$(pwd):g" -e "s:<user>:$USER:g" ./daemon/ti-3-get.service | sudo tee /etc/systemd/system/ti-3-get.service
-sed -e "s:<pwd>:$(pwd):g" -e "s:<user>:$USER:g" ./daemon/ti-3-update.service | sudo tee /etc/systemd/system/ti-3-update.service
+sed -e "s:<pwd>:$(pwd):g" -e "s:<user>:$USER:g" ./daemon/ti-3-get.service | tee /etc/systemd/system/ti-3-get.service
+sed -e "s:<pwd>:$(pwd):g" -e "s:<user>:$USER:g" ./daemon/ti-3-update.service | tee /etc/systemd/system/ti-3-update.service
 
 systemctl start ti-3-get.service
 systemctl enable ti-3-get.service
 
-sudo systemctl start ti-3-update.service
-sudo systemctl enable ti-3-update.service
+systemctl start ti-3-update.service
+systemctl enable ti-3-update.service
 
-sed -e "s:<pwd>:$(pwd):g" -e "s/<host>/$tiHostname/g" ./nginx.conf | sudo tee "/etc/nginx/sites-available/$tiHostname"
+sed -e "s:<pwd>:$(pwd):g" -e "s/<host>/$tiHostname/g" ./nginx.conf | tee "/etc/nginx/sites-available/$tiHostname"
 ln -s "/etc/nginx/sites-available/$tiHostname" "/etc/nginx/sites-enabled"
 systemctl restart nginx
